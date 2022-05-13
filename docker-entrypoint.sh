2270a8dae8182f3a886ad17ccc67e882f2791cbc
Tudor Amariei <tudor.amariei@code4.ro>
2022-05-13 15:35:42 +0300

    Change deployment credentials

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,8 @@
 if [[ ${ENVIRONMENT} != "production" ]] ; then
     uvicorn main:app --host 0.0.0.0 --port 80 --reload
 else
+    [ -d "/creds" ] || mkdir "/creds"
+    echo "${GOOGLE_CREDENTIALS}" > "${GOOGLE_APPLICATION_CREDENTIALS}"
+
     uvicorn main:app --host 0.0.0.0 --port 80
 fi

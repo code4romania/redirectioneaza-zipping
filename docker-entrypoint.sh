0c7f9f62051b7526b39a337985f55ad62ff742ed
Tudor Amariei <tudor.amariei@code4.ro>
2022-05-14 11:43:00 +0300

    Change the way credentials are added

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,11 @@
 if [[ ${ENVIRONMENT} != "production" ]] ; then
     uvicorn main:app --host 0.0.0.0 --port 80 --reload
 else
-    [ -d "/creds" ] || mkdir "/creds"
+    CREDENTIALS_DIRECTORY=$(dirname "${GOOGLE_APPLICATION_CREDENTIALS}")
+
+    [ -d "${CREDENTIALS_DIRECTORY}" ] || mkdir -p "${CREDENTIALS_DIRECTORY}"
+    touch "${GOOGLE_APPLICATION_CREDENTIALS}"
+
     echo "${GOOGLE_CREDENTIALS}" > "${GOOGLE_APPLICATION_CREDENTIALS}"
 
     uvicorn main:app --host 0.0.0.0 --port 80
